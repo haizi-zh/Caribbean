ec97d2d7655b0c838fb700d93ec9a2920be6e2ec
Demien Li <demien@appannie.com>
2015-01-31 13:45:58 +0800

    change user table

--- application/models/do/do_adminuser.php
+++ application/models/do/do_adminuser.php
@@ -8,25 +8,25 @@ class Do_adminuser extends CI_Model {
 	}
 
 	public function get_list(){
-		$query = $this->db->get('user');
+		$query = $this->db->get('cms_user');
 		return $query->result_array();
 	}
 	public function get_passwd($uname){
 		$this->db->select("id,power,password");
 		$this->db->where("username", $uname);
-		$query = $this->db->get("user");
+		$query = $this->db->get("cms_user");
 		return $query->row_array();
 	}
 	public function get_user_info($id){
 		$this->db->select("*");
 		$this->db->where("id", $id);
-		$query = $this->db->get("user");
+		$query = $this->db->get("cms_user");
 		return $query->row_array();
 	}
 }
 
 // CMS建表sql
-// create table user
+// create table cms_user
 // (
 //     id INT(20) NOT NULL AUTO_INCREMENT,
 //     username char(30) not null,
@@ -35,5 +35,5 @@ class Do_adminuser extends CI_Model {
 //     info text(5000) not null,
 //     PRIMARY KEY (id)
 // )default charset 'utf8';
-// INSERT INTO `user` (`id`, `username`, `password`, `power`, `info`) VALUES (NULL, 'keven', MD5('keven'), '0', '');
-// INSERT INTO `user` (`id`, `username`, `password`, `power`, `info`) VALUES (NULL, 'cc', MD5('cc'), '0', '');
+// INSERT INTO `cms_user` (`id`, `username`, `password`, `power`, `info`) VALUES (NULL, 'keven', MD5('keven'), '0', '');
+// INSERT INTO `cms_user` (`id`, `username`, `password`, `power`, `info`) VALUES (NULL, 'cc', MD5('cc'), '0', '');
