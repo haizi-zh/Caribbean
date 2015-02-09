023a49b3d495182f6effbf360ab7cc12b2ba7a6d
elainelulu <elainev_lu@163.com>
2015-02-09 18:35:39 +0800

    modify host

--- application/config/cimongo.php
+++ application/config/cimongo.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['host'] = "localhost";
+$config['host'] = "127.0.0.1";
 $config['port'] = 27017;
 $config['db'] = "admin";
 $config['user'] = "cms";
